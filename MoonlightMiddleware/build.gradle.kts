db8799bb0cdb49e1d495f1c7f653b5ceed6fbe9d
Ennio Visconti <ennio.visconti@gmail.com>
2022-02-18 12:07:44 +0100

    updated dependencies

--- MoonlightMiddleware/build.gradle.kts
+++ MoonlightMiddleware/build.gradle.kts
@@ -11,6 +11,11 @@ repositories {
     mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
 sonarqube {
     properties {
         property("sonar.projectKey", "oihanagarciaa_GBL-IoTMonitoringSTREL")
@@ -23,6 +28,8 @@ dependencies {
     implementation(files("libs/moonlight.jar"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.mockito:mockito-core:4.3.1")
+
 
     implementation("com.googlecode.json-simple:json-simple:1.1.1")
     implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
