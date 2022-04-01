424a03410d4b08a7506942e31329846f3c23c614
oihana.garciaa <oihana.garciaa@alumni.mondragon.edu>
2022-04-01 05:03:38 -0400

    Separate bluetooth stuff to another file

---
+++ zephyrWorkspace/thingy52OfficeMain/src/main.c
@@ -0,0 +1,33 @@
+/** Runtime Verification of spatio/temporal propperties over IoT networks
+ */
+#include "bluetooth.h"
+#include "sensors.h"
+
+#include <device.h>
+#include <devicetree.h>
+#include <zephyr.h>
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
+
+/* The main function */
+int main() {
+  int err;
+  
+  err = initialize_bluetooth();
+  if(err){
+      printk("Bluetooth init failed (err %d)\n", err);
+      return err;
+  }
+
+  register_callbacks();
+
+  err = setup_advertisement_parameters();
+  if (err) {
+    printk("Advertising failed to start (err %d)\n", err);
+    return err;
+  } else {
+    printk("Started advertising.\n");
+  }
+
+  return 0;
+}
